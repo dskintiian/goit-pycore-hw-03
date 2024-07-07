8cdc4bacc19ff8b41a1c008841755f40ea475324
Dmytro Skintiian <dmytro.skintiian@gmail.com>
2024-07-07 14:55:29 +0300

    Exceptions handling added

--- 01_get_days_from_today.py
+++ 01_get_days_from_today.py
@@ -4,10 +4,12 @@
 from datetime import datetime
 
 
-def get_days_from_today(date: str):
-    difference = datetime.now() - datetime.strptime(date, '%Y-%m-%d')
-
-    return difference.days
+def get_days_from_today(date: str) -> int or None:
+    try:
+        difference = datetime.now() - datetime.strptime(date, '%Y-%m-%d')
+        return difference.days
+    except ValueError as e:
+        print(f"Invalid date provided, use YYYY-MM-DD format for input date.\n### {e} ###")
 
 
 # past
@@ -16,3 +18,7 @@ print(get_days_from_today("2021-10-09"))
 print(get_days_from_today("2024-07-07"))
 # future
 print(get_days_from_today("2026-04-12"))
+# invalid
+print(get_days_from_today("2026-02-55"))
+print(get_days_from_today("yesterday"))
+print(get_days_from_today("Python is a programming language"))
